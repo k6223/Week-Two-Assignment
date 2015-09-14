5e212282d8a99608959ba65ecea177a2a60298d8
k6223 <kollin.schalhamer@myemail.indwes.edu>
2015-09-14 18:24:33 +0000

    Added conversions

---
+++ Conversion.py
@@ -0,0 +1,35 @@
+# Default shell for a Python 3.x program
+# Copy this and rename it to write your own code
+#
+__author__ = 'Kollin Schalhamer'
+
+# CIS-125
+# Name of program
+#
+# Brief description of what program does.
+
+# Declare Magic Numbers and Constants
+
+# LOOPCOUNTER = 2
+# name = "bob"
+
+# Write program code here
+
+#ask user if a temp in Fahrenheit
+#Convert to Celsius
+#(F-32) * 5 / 9
+
+#Output answer to user
+
+
+# Input
+
+F = input("Please enter a temp in Fahrenheit:")
+
+C = (F-32) * 5 / 9
+
+print("the temp" + "in Fahrenheit is eqaul to " + C + " Celsius")
+
+# Process
+
+# Output
---
+++ conversion.py
@@ -0,0 +1,5 @@
+F = input("Please enter a temp in Fahrenheit:")
+
+C = (F-32) * 5 / 9
+
+print("the temp" + "in Fahrenheit is eqaul to ", C ," Celsius")
