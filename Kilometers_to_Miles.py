cdb5d89a8ace6cdee7bcf65dae5722aad26a6f7d
k6223 <kollin.schalhamer@myemail.indwes.edu>
2015-09-16 18:43:43 +0000

    Kilometers to Miles done

---
+++ Kilometers_to_Miles.py
@@ -0,0 +1,21 @@
+
+#__author__ = 'Kollin Schalhamer'
+
+# CIS - 125 -82A
+# Kilometers_to_Miles.py
+#
+# Coverts Kilometers to Miles
+
+# Write program code here
+
+M= eval(input("Please enter a distance in Kilometers:" ))
+K = M * .62
+miles = M
+
+print("the distance ", M, " in Kilometers is eqaul to ", K ,"  Miles")
+
+# Input= 1 Kilometer
+
+# Process
+
+# Output= .62 Miles
