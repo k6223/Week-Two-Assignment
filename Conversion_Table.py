f7da10cebcb74226e4c4c4afee9f3a69ebd726cf
k6223 <kollin.schalhamer@myemail.indwes.edu>
2015-09-16 18:16:47 +0000

    Conversion Table updated

---
+++ Conversion_Table.py
@@ -0,0 +1,36 @@
+
+__author__ = 'Kollin Schalhamer'
+
+# CIS-125-82A
+# conversion.py
+#
+# Converts a table of degrees from Fahrenheit to Celsius
+
+# name = "Kollin"
+
+# Write program code here
+
+# Input
+
+for C in range(0,101,10):
+    F = C * (9 / 5) + 32
+
+    print("the temp ", F, " in Fahrenheit is eqaul to ", C ," Celsius")
+
+# Process
+
+# Output
+
+F = eval(input("Please enter a temp in Fahrenheit:"))
+
+('the temp ', 32, ' in Fahrenheit is eqaul to ', 0, ' Celsius')                                                                                                                                                                                      
+('the temp ', 42, ' in Fahrenheit is eqaul to ', 10, ' Celsius')                                                                                                                                                                                     
+('the temp ', 52, ' in Fahrenheit is eqaul to ', 20, ' Celsius')                                                                                                                                                                                     
+('the temp ', 62, ' in Fahrenheit is eqaul to ', 30, ' Celsius')                                                                                                                                                                                     
+('the temp ', 72, ' in Fahrenheit is eqaul to ', 40, ' Celsius')                                                                                                                                                                                     
+('the temp ', 82, ' in Fahrenheit is eqaul to ', 50, ' Celsius')                                                                                                                                                                                     
+('the temp ', 92, ' in Fahrenheit is eqaul to ', 60, ' Celsius')                                                                                                                                                                                     
+('the temp ', 102, ' in Fahrenheit is eqaul to ', 70, ' Celsius')                                                                                                                                                                                    
+('the temp ', 112, ' in Fahrenheit is eqaul to ', 80, ' Celsius')                                                                                                                                                                                    
+('the temp ', 122, ' in Fahrenheit is eqaul to ', 90, ' Celsius')                                                                                                                                                                                    
+('the temp ', 132, ' in Fahrenheit is eqaul to ', 100, ' Celsius')  
--- conversion.py
+++
@@ -1,5 +0,0 @@
-F = input("Please enter a temp in Fahrenheit:")
-
-C = (F-32) * 5 / 9
-
-print("the temp" + "in Fahrenheit is eqaul to ", C ," Celsius")
